74a5e8dd1d007e262aa1c5b8f5b7fac762da7dc6
elforjani3 <elforjani3@gmail.com>
2020-10-28 19:13:32 +0200

    FIx: Bill Entries validation

--- client/src/containers/Purchases/Bill/BillForm.js
+++ client/src/containers/Purchases/Bill/BillForm.js
@@ -103,7 +103,12 @@ function BillForm({
       .label(formatMessage({ id: 'note' })),
     entries: Yup.array().of(
       Yup.object().shape({
-        quantity: Yup.number().nullable(),
+        quantity: Yup.number()
+          .nullable()
+          .when(['rate'], {
+            is: (rate) => rate,
+            then: Yup.number().required(),
+          }),
         rate: Yup.number().nullable(),
         item_id: Yup.number()
           .nullable()
