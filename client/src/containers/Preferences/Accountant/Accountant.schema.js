8a9c9cf4983b9b4d5c4e2e0ddddec69d6431a6c1
elforjani3 <elforjani3@gmail.com>
2021-03-22 19:12:17 +0200

    fix(accountant) : accountant schema.

--- client/src/containers/Preferences/Accountant/Accountant.schema.js
+++ client/src/containers/Preferences/Accountant/Accountant.schema.js
@@ -2,8 +2,8 @@ import * as Yup from 'yup';
 
 const Schema = Yup.object().shape({
   accounting_basis: Yup.string().required(),
-  account_code_required: Yup.boolean(),
-  account_code_unique: Yup.boolean(),
+  account_code_required: Yup.boolean().nullable(),
+  account_code_unique: Yup.boolean().nullable(),
   deposit_account: Yup.number().nullable(),
   withdrawal_account: Yup.number().nullable(),
   advance_deposit: Yup.number().nullable(),
