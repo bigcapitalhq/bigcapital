282394c8fe9b72e046c8d781b0430bfa4d1350c3
elforjani3 <elforjani3@gmail.com>
2021-06-07 19:24:04 +0200

    feat: quick new options lang.

--- client/src/common/quickNewOptions.js
+++ client/src/common/quickNewOptions.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import { formatMessage } from 'services/intl';
+
 export default [
-  { path: 'invoices/new', name: 'Sale invoice' },
-  { path: 'bills//new', name: 'Purchase invoice' },
-  { path: 'make-journal-entry', name: 'Manual journal' },
-  { path: 'expenses/new', name: 'Expense'  },
-  { path: 'customers/new', name: 'Customer' },
-  { path: 'vendors/new', name: 'Vendor' },
+  { path: 'invoices/new', name: formatMessage({ id: 'sale_invoice' }) },
+  { path: 'bills//new', name: formatMessage({ id: 'purchase_invoice' }) },
+  { path: 'make-journal-entry', name: formatMessage({ id: 'manual_journal' }) },
+  { path: 'expenses/new', name: formatMessage({ id: 'expense' }) },
+  { path: 'customers/new', name: formatMessage({ id: 'customer' }) },
+  { path: 'vendors/new', name: formatMessage({ id: 'vendor' }) },
 ];
