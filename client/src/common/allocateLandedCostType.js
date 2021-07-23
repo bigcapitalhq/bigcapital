d0c2be90bf9562bb6d445ffd37f789eb0466e11b
elforjani3 <elforjani3@gmail.com>
2021-07-23 22:30:36 +0200

    feat : bill transaction delete alert.

--- client/src/common/allocateLandedCostType.js
+++ client/src/common/allocateLandedCostType.js
@@ -1,6 +1,6 @@
 import intl from 'react-intl-universal';
 
 export default [
-  { name: intl.get('bills'), value: 'bills' },
-  { name: intl.get('expenses'), value: 'expenses' },
-]
+  { name: intl.get('bills'), value: 'Bill' },
+  { name: intl.get('expenses'), value: 'Expense' },
+];
