f609cb30cf47a886f5cfeb4de169b63dfb40771e
a.bouhuolia <a.bouhuolia@live.com>
2021-08-04 22:23:32 +0200

    feat: dashboard error boundary localization.

--- client/src/components/Dashboard/DashboardErrorBoundary.js
+++ client/src/components/Dashboard/DashboardErrorBoundary.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Icon } from 'components';
+import { FormattedMessage as T, Icon } from 'components';
 
 export default function DashboardErrorBoundary({}) {
   return (
     <div class="dashboard__error-boundary">
-      <h1>Sorry about that! Something went wrong</h1>
-      <p>If the problem stuck, please <a href="#">contact us</a> as soon as possible.</p>
+      <h1><T id={'sorry_about_that_something_went_wrong'} /></h1>
+      <p><T id={'if_the_problem_stuck_please_contact_us_as_soon_as_possible'} /></p>
       <Icon icon="bigcapital" height={30} width={160} />
     </div>
   )
