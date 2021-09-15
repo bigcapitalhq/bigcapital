2e7e9d267520f2a9e3ff3771245795dfd2c1b601
elforjani3 <elforjani3@gmail.com>
2021-09-15 16:31:19 +0200

    fix: quick new options.

--- client/src/common/quickNewOptions.js
+++ client/src/common/quickNewOptions.js
@@ -2,7 +2,7 @@ import intl from 'react-intl-universal';
 
 export const getQuickNewActions = () => [
   { path: 'invoices/new', name: intl.get('sale_invoice') },
-  { path: 'bills//new', name: intl.get('purchase_invoice') },
+  { path: 'bills/new', name: intl.get('purchase_invoice') },
   { path: 'make-journal-entry', name: intl.get('manual_journal') },
   { path: 'expenses/new', name: intl.get('expense') },
   { path: 'customers/new', name: intl.get('customer') },
