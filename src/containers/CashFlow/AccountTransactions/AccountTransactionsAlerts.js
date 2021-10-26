c462681c703083a9cd3a7d447143da60f7264c24
elforjani13 <39470382+elforjani13@users.noreply.github.com>
2021-10-26 19:34:16 +0200

    feat: account transaction alerts.

--- src/containers/CashFlow/AccountTransactions/AccountTransactionsAlerts.js
+++ src/containers/CashFlow/AccountTransactions/AccountTransactionsAlerts.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 import AccountDeleteTransactionAlert from '../../Alerts/CashFlow/AccountDeleteTransactionAlert';
+import ReceiptsAlerts from '../../Sales/Receipts/ReceiptsAlerts';
+import ManualJournalsAlerts from '../../Accounting/JournalsLanding/ManualJournalsAlerts';
+import ExpensesAlerts from '../../Expenses/ExpensesAlerts';
+import PaymentReceiveAlerts from '../../Sales/PaymentReceives/PaymentReceiveAlerts';
+import PaymentMadesAlerts from '../../Purchases/PaymentMades/PaymentMadesAlerts';
 
 /**
  * Account transaction alert.
@@ -9,6 +14,11 @@ export default function AccountTransactionsAlerts() {
   return (
     <div>
       <AccountDeleteTransactionAlert name={'account-transaction-delete'} />
+      <ReceiptsAlerts />
+      <ExpensesAlerts />
+      <ManualJournalsAlerts />
+      <PaymentReceiveAlerts />
+      <PaymentMadesAlerts />
     </div>
   );
 }
