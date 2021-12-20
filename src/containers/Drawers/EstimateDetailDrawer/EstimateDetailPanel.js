a0098382e780723f3e4887dea9b6812577f86c15
a.bouhuolia <a.bouhuolia@live.com>
2021-12-20 14:12:19 +0200

    feat: optimize style of sale estimate details.

--- src/containers/Drawers/EstimateDetailDrawer/EstimateDetailPanel.js
+++ src/containers/Drawers/EstimateDetailDrawer/EstimateDetailPanel.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import clsx from 'classnames';
 
-import { Card } from 'components';
+import { CommercialDocBox } from 'components';
 
-import EstimateDetailActionsBar from './EstimateDetailActionsBar';
 import EstimateDetailHeader from './EstimateDetailHeader';
 import EstimateDetailTable from './EstimateDetailTable';
-
-import EstimateDetailsCls from 'style/components/Drawers/EstimateDetails.module.scss';
 import EstimateDetailFooter from './EstimateDetailFooter';
 
 export default function EstimateDetailTab() {
   return (
-    <div className={clsx(EstimateDetailsCls.detail_panel)}>
-      <EstimateDetailActionsBar />
-
-      <Card>
-        <EstimateDetailHeader />
-        <EstimateDetailTable />
-        <EstimateDetailFooter />
-      </Card>
-    </div>
+    <CommercialDocBox>
+      <EstimateDetailHeader />
+      <EstimateDetailTable />
+      <EstimateDetailFooter />
+    </CommercialDocBox>
   );
 }
