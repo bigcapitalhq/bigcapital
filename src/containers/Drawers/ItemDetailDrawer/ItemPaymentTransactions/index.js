da3564d3156027d38e00922b1ad2b4eaffe47db9
elforjani13 <39470382+elforjani13@users.noreply.github.com>
2021-12-21 10:43:04 +0200

    fix: handle switch button item transaction.

--- src/containers/Drawers/ItemDetailDrawer/ItemPaymentTransactions/index.js
+++ src/containers/Drawers/ItemDetailDrawer/ItemPaymentTransactions/index.js
@@ -1,30 +1,22 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Card, FormattedMessage as T } from 'components';
+import { Card } from 'components';
 import { ItemManuTransaction } from './utils';
 import { useItemDetailDrawerContext } from '../ItemDetailDrawerProvider';
 import ItemPaymentTransactionContent from './ItemPaymentTransactionContent';
 
 export const ItemPaymentTransactions = () => {
-  const { value } = useItemDetailDrawerContext();
+  const { value, setValue } = useItemDetailDrawerContext();
+
+  // handle item change.
+  const handleItemChange = (item) => {
+    setValue(item);
+  };
 
   return (
     <Card>
-      <ItemManuTransactions>
-        <T id={'item.drawer_transactions_by'} />
-        <ItemManuTransaction />
-      </ItemManuTransactions>
+      <ItemManuTransaction onChange={handleItemChange} />
       <ItemPaymentTransactionContent tansactionType={value} />
     </Card>
   );
 };
 
-const ItemManuTransactions = styled.div`
-  display: flex;
-  align-items: center;
-  color: #727983;
-  .bp3-button {
-    padding-left: 6px;
-    font-weight: 500;
-  }
-`;
