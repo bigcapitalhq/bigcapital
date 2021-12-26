8ea92513fec800012c71a808b6ed0f0de7c5787a
elforjani13 <39470382+elforjani13@users.noreply.github.com>
2021-12-26 13:26:10 +0200

    feat: receipt detail footer.

--- src/containers/Drawers/ReceiptDetailDrawer/ReceiptDetailFooter.js
+++ src/containers/Drawers/ReceiptDetailDrawer/ReceiptDetailFooter.js
@@ -1,51 +1,35 @@
 import React from 'react';
-import styled from 'styled-components';
-
 import {
+  CommercialDocFooter,
   T,
-  TotalLines,
-  TotalLine,
-  TotalLineBorderStyle,
-  TotalLineTextStyle,
-  FormatNumber,
+  If,
+  DetailsMenu,
+  DetailItem,
 } from 'components';
+
 import { useReceiptDetailDrawerContext } from './ReceiptDetailDrawerProvider';
 
 /**
- * Receipts read-only details footer.
+ * Receipt details footer
+ * @returns {React.JSX}
  */
-export function ReceiptDetailFooter() {
+export default function ReceiptDetailFooter() {
   const { receipt } = useReceiptDetailDrawerContext();
 
   return (
-    <ReceiptDetailsFooterRoot>
-      <ReceiptTotalLines labelColWidth={'180px'} amountColWidth={'180px'}>
-        <TotalLine
-          title={<T id={'receipt.details.subtotal'} />}
-          value={<FormatNumber value={receipt.amount} />}
-        />
-        <TotalLine
-          title={<T id={'receipt.details.total'} />}
-          value={receipt.formatted_amount}
-          borderStyle={TotalLineBorderStyle.DoubleDark}
-          textStyle={TotalLineTextStyle.Bold}
-        />
-        <TotalLine
-          title={<T id={'receipt.details.payment_amount'} />}
-          value={receipt.formatted_amount}
-          borderStyle={TotalLineBorderStyle.DoubleDark}
-        />
-        <TotalLine
-          title={<T id={'receipt.details.due_amount'} />}
-          value={'0'}
-        />
-      </ReceiptTotalLines>
-    </ReceiptDetailsFooterRoot>
+    <CommercialDocFooter>
+      <DetailsMenu direction={'horizantal'} minLabelSize={'180px'}>
+        <If condition={receipt.statement}>
+          <DetailItem label={<T id={'receipt.details.statement'} />}>
+            {receipt.statement}
+          </DetailItem>
+        </If>
+        <If condition={receipt.receipt_message}>
+          <DetailItem label={<T id={'receipt.details.receipt_message'} />}>
+            {receipt.receipt_message}
+          </DetailItem>
+        </If>
+      </DetailsMenu>
+    </CommercialDocFooter>
   );
 }
-
-export const ReceiptDetailsFooterRoot = styled.div``;
-
-export const ReceiptTotalLines = styled(TotalLines)`
-  margin-left: auto;
-`;
