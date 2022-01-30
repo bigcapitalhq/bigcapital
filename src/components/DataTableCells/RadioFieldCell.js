8c594b991cfb768401206dbc6075af04d63a6c21
elforjani13 <39470382+elforjani13@users.noreply.github.com>
2022-01-30 18:04:56 +0200

    feat(warehouse) add warehouse propover.

---
+++ src/components/DataTableCells/RadioFieldCell.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import classNames from 'classnames';
+import { FormGroup, Intent, Classes, Radio } from '@blueprintjs/core';
+
+const RadioEditableCell = ({
+  row: { index, original },
+  column: { id, radioProps },
+  cell: { value: initialValue },
+  payload,
+}) => {
+  const [value, setValue] = React.useState(initialValue);
+
+  const onChange = (e) => {
+    const newValue = e.target.checked;
+    debugger;
+    setValue(newValue);
+    payload.updateData(index, id, newValue);
+  };
+
+  React.useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
+
+  const error = payload.errors?.[index]?.[id];
+
+  return (
+    <FormGroup
+      intent={error ? Intent.DANGER : null}
+      className={classNames(Classes.FILL)}
+    >
+      <Radio
+        value={value}
+        label={'Warehouse #1'}
+        onChange={onChange}
+        checked={initialValue}
+        minimal={true}
+        className="ml2"
+        {...radioProps}
+      />
+    </FormGroup>
+  );
+};
+
+export default RadioEditableCell;
