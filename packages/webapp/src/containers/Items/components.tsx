2263cf5657d327dcf3278651807915d2fd078b52
a.bouhuolia <a.bouhuolia@live.com>
2023-02-16 22:34:14 +0200

    fix(webapp): add icon to duplicate item of items context menu

--- packages/webapp/src/containers/Items/components.tsx
+++ packages/webapp/src/containers/Items/components.tsx
@@ -105,7 +105,7 @@ export function ItemsActionMenuList({
       </Can>
       <Can I={ItemAction.Create} a={AbilitySubject.Item}>
         <MenuItem
-          icon={<Icon icon="duplicate-16" />}
+          icon={<Icon icon="content-copy" iconSize={16} />}
           text={intl.get('duplicate')}
           onClick={safeCallback(onDuplicate, original)}
         />
