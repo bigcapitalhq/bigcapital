4d44ce4c7f58f1d44d25b01d57c29dd51075a0fc
a.bouhuolia <a.bouhuolia@live.com>
2023-05-04 21:29:12 +0200

    fix(webapp): some flag icons are missing

--- packages/webapp/src/components/Tags/FlagIcon.tsx
+++ packages/webapp/src/components/Tags/FlagIcon.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 export const FlagIcon = ({ currencyCode, className }) => {
-  const source = `/icons/flags/${currencyCode}.svg`;
+  const source = `/icons/currency-flags/${currencyCode}.svg`;
 
   return <img alt="flag" src={source} className={className} />;
 };
