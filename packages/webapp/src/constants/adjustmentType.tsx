97b223e8bab6f57326860bc71b0b686dd219edb0
Ahmed Bouhuolia <a.bouhuolia@gmail.com>
2023-06-15 19:58:17 +0200

    fix(webapp): adjustment type options do not show up

--- packages/webapp/src/constants/adjustmentType.tsx
+++
@@ -1,7 +0,0 @@
-// @ts-nocheck
-import intl from 'react-intl-universal';
-
-export default [
-  { name: intl.get('decrement'), value: 'decrement' },
-  { name: intl.get('increment'), value: 'increment' },
-]
