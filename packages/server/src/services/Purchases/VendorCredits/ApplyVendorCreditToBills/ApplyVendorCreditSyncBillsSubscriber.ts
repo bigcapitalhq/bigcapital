d177e100024e18fda000c960ba4ff7d3647bcc28
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-06-19 01:37:51 -0400

    spelling: deleted
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- packages/server/src/services/Purchases/VendorCredits/ApplyVendorCreditToBills/ApplyVendorCreditSyncBillsSubscriber.ts
+++ packages/server/src/services/Purchases/VendorCredits/ApplyVendorCreditToBills/ApplyVendorCreditSyncBillsSubscriber.ts
@@ -48,7 +48,7 @@ export default class ApplyVendorCreditSyncBillsSubscriber {
 
   /**
    * Decrement credited amount of applied bills once the vendor credit
-   * transaction delted.
+   * transaction deleted.
    * @param {IVendorCreditApplyToBillDeletedPayload} payload
    */
   private decrementAppliedBillsOnceCreditDeleted = async ({
