edd290165ad941a2d073f0c5bc243376ff17fa3d
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-06-19 01:39:12 -0400

    spelling: ignored
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- packages/webapp/src/components/Datatable/TableCell.tsx
+++ packages/webapp/src/components/Datatable/TableCell.tsx
@@ -9,7 +9,7 @@ import TableContext from './TableContext';
 import { saveInvoke, ignoreEventFromSelectors } from '@/utils';
 import { isCellLoading } from './utils';
 
-const ROW_CLICK_SELECTORS_INGORED = ['.expand-toggle', '.selection-checkbox'];
+const ROW_CLICK_SELECTORS_IGNORED = ['.expand-toggle', '.selection-checkbox'];
 
 /**
  * Table cell.
@@ -54,7 +54,7 @@ export default function TableCell({ cell, row, index }) {
   }
   // Handle cell click action.
   const handleCellClick = (event) => {
-    if (ignoreEventFromSelectors(event, ROW_CLICK_SELECTORS_INGORED)) {
+    if (ignoreEventFromSelectors(event, ROW_CLICK_SELECTORS_IGNORED)) {
       return;
     }
     saveInvoke(onCellClick, cell, event);
