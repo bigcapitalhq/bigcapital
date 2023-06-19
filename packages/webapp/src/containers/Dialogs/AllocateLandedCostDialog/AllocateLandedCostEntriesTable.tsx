f29ea901838a38ba161b4e3f8a039c4e14dafed9
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-06-19 03:34:14 -0400

    spelling: landed
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- packages/webapp/src/containers/Dialogs/AllocateLandedCostDialog/AllocateLandedCostEntriesTable.tsx
+++ packages/webapp/src/containers/Dialogs/AllocateLandedCostDialog/AllocateLandedCostEntriesTable.tsx
@@ -29,7 +29,7 @@ export default function AllocateLandedCostEntriesTable({
   );
 
   return (
-    <AllocateLandeedCostEntriesEditableTable
+    <AllocateLandedCostEntriesEditableTable
       columns={columns}
       data={entries}
       payload={{
@@ -40,7 +40,7 @@ export default function AllocateLandedCostEntriesTable({
   );
 }
 
-export const AllocateLandeedCostEntriesEditableTable = styled(
+export const AllocateLandedCostEntriesEditableTable = styled(
   DataTableEditable,
 )`
   .table {
