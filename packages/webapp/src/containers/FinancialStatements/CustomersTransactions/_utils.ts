a630e8a6129840f9885f97f786b5f9dec2435968
Ahmed Bouhuolia <a.bouhuolia@gmail.com>
2023-08-28 20:53:52 +0200

    fix: change the default from/to dates of customers/vendors transactions

--- packages/webapp/src/containers/FinancialStatements/CustomersTransactions/_utils.ts
+++ packages/webapp/src/containers/FinancialStatements/CustomersTransactions/_utils.ts
@@ -1,6 +1,11 @@
+// @ts-nocheck
 import * as Yup from 'yup';
 import intl from 'react-intl-universal';
 import moment from 'moment';
+import { transformToForm } from '@/utils';
+import { castArray } from 'lodash';
+import { useMemo } from 'react';
+import { useAppQueryString } from '@/hooks';
 
 export const getCustomersTransactionsQuerySchema = () => {
   return Yup.object().shape({
@@ -13,7 +18,31 @@ export const getCustomersTransactionsQuerySchema = () => {
 };
 
 export const getCustomersTransactionsDefaultQuery = () => ({
-  fromDate: moment().startOf('month').toDate(),
-  toDate: moment().toDate(),
+  fromDate: moment().startOf('month').format('YYYY-MM-DD'),
+  toDate: moment().format('YYYY-MM-DD'),
   customersIds: [],
+  filterByOption: 'with-transactions',
 });
+
+const parseCustomersTransactionsQuery = (query) => {
+  const defaultQuery = getCustomersTransactionsDefaultQuery();
+
+  const transformedQuery = {
+    ...defaultQuery,
+    ...transformToForm(query, defaultQuery),
+  };
+  return {
+    ...transformedQuery,
+    customersIds: castArray(transformedQuery.customersIds),
+  };
+};
+
+export const useCustomersTransactionsQuery = () => {
+  const [locationQuery, setLocationQuery] = useAppQueryString();
+
+  const query = useMemo(
+    () => parseCustomersTransactionsQuery(locationQuery),
+    [locationQuery],
+  );
+  return [query, setLocationQuery];
+};
--- packages/webapp/src/containers/FinancialStatements/VendorsTransactions/_utils.ts
+++ packages/webapp/src/containers/FinancialStatements/VendorsTransactions/_utils.ts
@@ -1,6 +1,10 @@
+// @ts-nocheck
 import * as Yup from 'yup';
 import intl from 'react-intl-universal';
 import moment from 'moment';
+import { useMemo } from 'react';
+import { transformToForm } from '@/utils';
+import { useAppQueryString } from '@/hooks';
 
 /**
  * The validation schema of vendors transactions.
@@ -19,7 +23,35 @@ export const getVendorTransactionsQuerySchema = () => {
  * Retrieves the default query of vendors transactions.
  */
 export const getVendorsTransactionsDefaultQuery = () => ({
-  fromDate: moment().startOf('month').toDate(),
-  toDate: moment().toDate(),
+  fromDate: moment().startOf('month').format('YYYY-MM-DD'),
+  toDate: moment().format('YYYY-MM-DD'),
   vendorsIds: [],
 });
+
+/**
+ * Parses the query of vendors transactions.
+ */
+const parseVendorsTransactionsQuery = (query) => {
+  const defaultQuery = getVendorsTransactionsDefaultQuery();
+  const transformed = {
+    ...defaultQuery,
+    ...transformToForm(query, defaultQuery),
+  };
+  return {
+    ...transformed,
+    vendorsIds: transformed.vendorsIds ? transformed.vendorsIds : [],
+  };
+};
+
+/**
+ * Retrieves the query of vendors transactions.
+ */
+export const useVendorsTransactionsQuery = () => {
+  const [locationQuery, setLocationQuery] = useAppQueryString();
+
+  const query = useMemo(
+    () => parseVendorsTransactionsQuery(locationQuery),
+    [locationQuery],
+  );
+  return [query, setLocationQuery];
+};
