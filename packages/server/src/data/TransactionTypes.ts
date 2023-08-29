7f746b96c828bd0ff2d96fec37a6b8fa9fd52cd4
Ahmed Bouhuolia <a.bouhuolia@gmail.com>
2023-08-29 03:05:19 +0200

    chore: remove `/data` directory from git ignored dirs

---
+++ packages/server/src/data/TransactionTypes.ts
@@ -0,0 +1,28 @@
+export const TransactionTypes = {
+  SaleInvoice: 'Sale invoice',
+  SaleReceipt: 'Sale receipt',
+  PaymentReceive: 'Payment receive',
+  Bill: 'Bill',
+  BillPayment: 'Payment made',
+  VendorOpeningBalance: 'Vendor opening balance',
+  CustomerOpeningBalance: 'Customer opening balance',
+  InventoryAdjustment: 'Inventory adjustment',
+  ManualJournal: 'Manual journal',
+  Journal: 'Manual journal',
+  Expense: 'Expense',
+  OwnerContribution: 'Owner contribution',
+  TransferToAccount: 'Transfer to account',
+  TransferFromAccount: 'Transfer from account',
+  OtherIncome: 'Other income',
+  OtherExpense: 'Other expense',
+  OwnerDrawing: 'Owner drawing',
+  InvoiceWriteOff: 'Invoice write-off',
+
+  CreditNote: 'transaction_type.credit_note',
+  VendorCredit: 'transaction_type.vendor_credit',
+
+  RefundCreditNote: 'transaction_type.refund_credit_note',
+  RefundVendorCredit: 'transaction_type.refund_vendor_credit',
+
+  LandedCost: 'transaction_type.landed_cost',
+};
