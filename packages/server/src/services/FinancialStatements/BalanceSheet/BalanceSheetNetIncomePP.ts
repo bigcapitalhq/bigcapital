078a7ea51cabad994389b7c014cca5410275a658
Ahmed Bouhuolia <a.bouhuolia@gmail.com>
2023-10-28 01:57:31 +0200

    fix: change Dockerfile files with new pnpm (#278)

--- packages/server/src/services/FinancialStatements/BalanceSheet/BalanceSheetNetIncomePP.ts
+++ packages/server/src/services/FinancialStatements/BalanceSheet/BalanceSheetNetIncomePP.ts
@@ -72,3 +72,4 @@ export const BalanceSheetNetIncomePP = (Base: any) =>
         this.assocPreviousPeriodNetIncomeNode
       )(node);
     };
+  };
--- packages/webapp/craco.config.js
+++ packages/webapp/craco.config.js
@@ -5,5 +5,10 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+    configure: {
+      resolve: {
+        fallback: { path: require.resolve('path-browserify') },
+      },
+    },
   },
 };
