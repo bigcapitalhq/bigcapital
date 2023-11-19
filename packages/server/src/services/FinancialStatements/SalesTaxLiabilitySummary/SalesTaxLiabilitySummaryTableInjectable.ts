bb6ac97d5abc247dfc7bf11d472d1aef25d7c386
Ahmed Bouhuolia <a.bouhuolia@gmail.com>
2023-11-19 22:36:27 +0200

    feat: export csv/xlsx sales tax liability summary report

---
+++ packages/server/src/services/FinancialStatements/SalesTaxLiabilitySummary/SalesTaxLiabilitySummaryTableInjectable.ts
@@ -0,0 +1,35 @@
+import { Inject, Service } from 'typedi';
+import { SalesTaxLiabilitySummaryQuery } from '@/interfaces/SalesTaxLiabilitySummary';
+import { SalesTaxLiabilitySummaryTable } from './SalesTaxLiabilitySummaryTable';
+import { SalesTaxLiabilitySummaryService } from './SalesTaxLiabilitySummaryService';
+
+@Service()
+export class SalesTaxLiabilitySummaryTableInjectable {
+  @Inject()
+  private salesTaxLiability: SalesTaxLiabilitySummaryService;
+
+  /**
+   * Retrieve sales tax liability summary table.
+   * @param {number} tenantId
+   * @param {SalesTaxLiabilitySummaryQuery} query
+   * @returns
+   */
+  public async table(tenantId: number, query: SalesTaxLiabilitySummaryQuery) {
+    const report = await this.salesTaxLiability.salesTaxLiability(
+      tenantId,
+      query
+    );
+    // Creates the sales tax liability summary table.
+    const table = new SalesTaxLiabilitySummaryTable(report.data, query);
+
+    return {
+      table: {
+        rows: table.tableRows(),
+        columns: table.tableColumns(),
+      },
+      data: report.data,
+      query: report.query,
+      meta: report.meta,
+    };
+  }
+}
