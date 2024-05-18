1227111faebd1ca602369b9e70be8b58bbaffd25
Ahmed Bouhuolia <a.bouhuolia@gmail.com>
2024-05-18 15:44:53 +0200

    fix: typo

--- packages/webapp/src/containers/Setup/SetupSubscription/SubscriptionPlansSection.tsx
+++ packages/webapp/src/containers/Setup/SetupSubscription/SubscriptionPlansSection.tsx
@@ -1,10 +1,8 @@
 // @ts-nocheck
-import { T } from '@/components';
-
+import { Callout } from '@blueprintjs/core';
 import { SubscriptionPlans } from './SubscriptionPlan';
 import withPlans from '../../Subscriptions/withPlans';
 import { compose } from '@/utils';
-import { Callout, Intent } from '@blueprintjs/core';
 
 /**
  * Billing plans.
@@ -15,10 +13,11 @@ function SubscriptionPlansSectionRoot({ plans }) {
       <Callout
         style={{ marginBottom: '1.5rem' }}
         icon={null}
-        title={'Early Adaptors Plan'}
+        title={'Early Adopter Plan'}
       >
-        We're looking for 200 early adaptors, when you subscribe you'll get
-        the full features and unlimited users for a year regardless of the subscribed plan.
+        We're looking for 200 early adopters, when you subscribe you'll get the
+        full features and unlimited users for a year regardless of the
+        subscribed plan.
       </Callout>
       <SubscriptionPlans plans={plans} />
     </section>
