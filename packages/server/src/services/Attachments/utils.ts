6caa1311fd117d4a5e7624a207b48f0aa5e01e37
Ahmed Bouhuolia <a.bouhuolia@gmail.com>
2024-10-01 15:09:21 +0200

    fix: Getting uploaded object uri

--- packages/server/src/services/Attachments/utils.ts
+++ packages/server/src/services/Attachments/utils.ts
@@ -1,7 +1,9 @@
 import path from 'path';
 import config from '@/config';
 
-
 export const getUploadedObjectUri = (objectKey: string) => {
-  return path.join(config.s3.endpoint, config.s3.bucket, objectKey);
-}
+  return new URL(
+    path.join(config.s3.bucket, objectKey),
+    config.s3.endpoint
+  ).toString();
+};
