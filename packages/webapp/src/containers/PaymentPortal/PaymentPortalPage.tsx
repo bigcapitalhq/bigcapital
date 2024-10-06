3358ce58bc2748d2178443b35e405746baf6a059
Ahmed Bouhuolia <a.bouhuolia@gmail.com>
2024-10-06 22:21:12 +0200

    feat: Change the document title of the payment page

--- packages/webapp/src/containers/PaymentPortal/PaymentPortalPage.tsx
+++ packages/webapp/src/containers/PaymentPortal/PaymentPortalPage.tsx
@@ -1,10 +1,11 @@
 import { useParams } from 'react-router-dom';
-import { PaymentPortal } from './PaymentPortal';
-import { PaymentPortalBoot } from './PaymentPortalBoot';
+import { Helmet } from 'react-helmet';
 import BodyClassName from 'react-body-classname';
-import styles from './PaymentPortal.module.scss';
+import { PaymentPortal } from './PaymentPortal';
+import { PaymentPortalBoot, usePaymentPortalBoot } from './PaymentPortalBoot';
 import { PaymentInvoicePreviewDrawer } from './drawers/PaymentInvoicePreviewDrawer/PaymentInvoicePreviewDrawer';
 import { DRAWERS } from '@/constants/drawers';
+import styles from './PaymentPortal.module.scss';
 
 export default function PaymentPortalPage() {
   const { linkId } = useParams<{ linkId: string }>();
@@ -12,9 +13,26 @@ export default function PaymentPortalPage() {
   return (
     <BodyClassName className={styles.rootBodyPage}>
       <PaymentPortalBoot linkId={linkId}>
+        <PaymentPortalHelmet />
         <PaymentPortal />
         <PaymentInvoicePreviewDrawer name={DRAWERS.PAYMENT_INVOICE_PREVIEW} />
       </PaymentPortalBoot>
     </BodyClassName>
+  );
+}
+
+/**
+ * Renders the document title of the current payment page.
+ * @returns {React.ReactNode}
+ */
+function PaymentPortalHelmet() {
+  const { sharableLinkMeta } = usePaymentPortalBoot();
+
+  return (
+    <Helmet>
+      <title>
+        {sharableLinkMeta?.invoiceNo} | {sharableLinkMeta?.organization?.name}
+      </title>
+    </Helmet>
   );
 }
