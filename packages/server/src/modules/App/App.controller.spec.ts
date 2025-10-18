e548e7dc4a3b70ff2eb3362aa7ceb07f84b6c08f
Ahmed Bouhuolia <a.bouhuolia@gmail.com>
2025-10-18 18:48:03 +0200

    feat: typecheck gh action

--- packages/server/src/modules/App/App.controller.spec.ts
+++ packages/server/src/modules/App/App.controller.spec.ts
@@ -16,7 +16,6 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
     });
   });
 });
