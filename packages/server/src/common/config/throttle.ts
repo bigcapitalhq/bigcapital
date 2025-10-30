27722ebb54cb7fe9a6a3757b8731c519b9510d90
Ahmed Bouhuolia <a.bouhuolia@gmail.com>
2025-10-30 22:05:40 +0200

    fix: update global throttle TTL to 60000

--- packages/server/src/common/config/throttle.ts
+++ packages/server/src/common/config/throttle.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('throttle', () => ({
   global: {
-    ttl: parseInt(process.env.THROTTLE_GLOBAL_TTL ?? '60', 10),
+    ttl: parseInt(process.env.THROTTLE_GLOBAL_TTL ?? '60000', 10),
     limit: parseInt(process.env.THROTTLE_GLOBAL_LIMIT ?? '100', 10),
   },
   auth: {
