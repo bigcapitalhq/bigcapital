dc6cf13a3e686ae9abf1c253a327bc29a494f1fb
Ahmed Bouhuolia <a.bouhuolia@gmail.com>
2025-11-25 21:34:17 +0200

    fix: wdyr error with vite

--- packages/webapp/src/wdyr.ts
+++ packages/webapp/src/wdyr.ts
@@ -1,8 +1,8 @@
+import React from 'react';
+import whyDidYouRender from '@welldone-software/why-did-you-render';
 
 if (process.env.NODE_ENV === 'development') {
-  import('@welldone-software/why-did-you-render').then(({ default: whyDidYouRender }) => {
-    if (whyDidYouRender) {
-      whyDidYouRender(React, { trackAllPureComponents: false });
-    }
+  whyDidYouRender(React, {
+    trackAllPureComponents: true,
   });
 }
