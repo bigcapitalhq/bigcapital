d7331554ad64ff1de93d55eb230ae54098dbecc2
Ahmed Bouhuolia <a.bouhuolia@gmail.com>
2026-01-21 13:07:03 +0200

    hotbug(server): interceptors order

--- packages/server/src/common/interceptors/to-json.interceptor.ts
+++ packages/server/src/common/interceptors/to-json.interceptor.ts
@@ -5,15 +5,18 @@ import {
   NestInterceptor,
 } from '@nestjs/common';
 import { Observable, map } from 'rxjs';
-import { mapValues, mapValuesDeep } from '@/utils/deepdash';
+import { mapValuesDeep } from '@/utils/deepdash';
 
 @Injectable()
 export class ToJsonInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map((data) => {
+        if (data === null || data === undefined) {
+          return data;
+        }
         return mapValuesDeep(data, (value) => {
-          if (value && typeof value.toJSON === 'function') {
+          if (value !== null && value !== undefined && typeof value.toJSON === 'function') {
             return value.toJSON();
           }
           return value;
