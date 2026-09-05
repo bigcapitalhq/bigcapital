2c11fe9a374535ee74e43192a8d47b8b11d4ac63
Ahmed Bouhuolia <a.bouhuolia@gmail.com>
2026-09-05 17:23:08 +0200

    fix(server): restore objection type reference in cli

--- packages/server/src/cli.ts
+++ packages/server/src/cli.ts
@@ -1,4 +1,5 @@
-import './common/types/Objection';
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./common/types/Objection.d.ts" />
 import { CommandFactory } from 'nest-commander';
 import { CLIModule } from './modules/CLI/CLI.module';
 
